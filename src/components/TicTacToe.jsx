d51d04dc53ca84c06846e1e4627af1b619cd2871
Mohammed Numan Raza <89739573+Numan71417@users.noreply.github.com>
2023-09-17 12:04:11 +0530

    Update TicTacToe.jsx

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -207,12 +207,12 @@ function TicTacToe( {handleMode, mode, apikey}) {
           <button style={{background:'none',border:'none',cursor:'pointer'}} onClick={soundHandle}><img src={soundIcon} width={'20px'} alt=""  className={mode} style={{background:'none',  filter:'brightness(2)}'}}
     /></button>
       </div>
-        
+      </div>
+
         <div className="turn">
           {playerTurn}'s Turn 
       </div>
-        
-      </div>
+      
       <Board
         playerTurn={playerTurn}
         tiles={tiles}
